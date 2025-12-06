a32f9f0538a9ef7c3bdbd65d06d832c4819d4067
assiazeghmar <zeghmarassia>
2025-12-06 17:43:16 +0100

    frontend setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center" >
+      <h1>test</h1>
+    </div>
+  );
+}
+
+export default App;
