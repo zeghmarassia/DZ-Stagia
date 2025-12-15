10cd655ec5b0830cde6bbbab648ea4759fd1a7a1
assiazeghmar <zeghmarassia>
2025-12-15 08:42:58 +0100

    implement signup and login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
-import Homepage from "./pages/Homepage.jsx"
-function App() {
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import Homepage from './pages/Homepage';
+import Login from './pages/Login';
+import SignupPage from './pages/SignupPage';
 
+function App() {
   return (
-    <div>
-      <Homepage/>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/login" element={<Login />} />
+        
+        {/* Redirect generic /signup to the student route by default */}
+        <Route path="/signup" element={<Navigate to="/student/signup" replace />} />
+        
+        {/* UPDATED ROUTES to match Backend */}
+        <Route path="/student/signup" element={<SignupPage type="student" />} />
+        <Route path="/company/signup" element={<SignupPage type="company" />} />
+      </Routes>
+    </Router>
   );
 }
 
