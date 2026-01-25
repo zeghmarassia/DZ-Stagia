9cc4a9a9f0e027c2edbd2abb8f4c66b8437d642c
assiazeghmar <zeghmarassia>
2026-01-25 13:49:14 +0100

    fix company page

--- frontend/src/pages/Companies.jsx
+++ frontend/src/pages/Companies.jsx
@@ -123,7 +123,6 @@ const Companies = () => {
 
   return (
     <div className="min-h-screen bg-slate-50 font-sans">
-      <Navbar /> 
 
       <div className="bg-white border-b border-gray-200 pt-16 pb-12">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
