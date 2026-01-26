32ccc85f2b8ec2d705bb8106ab1f84794e62711b
ZEGHMAR ASSIA <90804842+zeghmarassia@users.noreply.github.com>
2026-01-26 15:00:48 +0100

    fix admin login redirection

--- frontend/src/pages/AdminLoginPage.jsx
+++ frontend/src/pages/AdminLoginPage.jsx
@@ -34,7 +34,7 @@ const AdminLoginPage = () => {
     }));
 
     // Redirect to the admin dashboard
-    navigate('/admin/admin/stats');
+    navigate('/admin/dashboard');
   } catch (err) {
     // Handle errors using the response from axiosInstance
     setError(err.response?.data?.message || "Identifiants administrateur invalides.");
